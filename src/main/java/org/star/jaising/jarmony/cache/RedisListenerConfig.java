bcbb236f232e3046e640fd81656dc2ddbd63f39e
Jaising <swaggerjaising@gmail.com>
2020-07-10 09:36:17 +0800

    feat: SpringBoot with Redis

---
+++ src/main/java/org/star/jaising/jarmony/cache/RedisListenerConfig.java
@@ -0,0 +1,22 @@
+package org.star.jaising.jarmony.cache;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @author zhengjiaxing
+ * @description 监听redis
+ * @date 2020/7/8
+ */
+@Configuration
+public class RedisListenerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+}
